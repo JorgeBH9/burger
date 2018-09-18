99098ec30f6381b7dd5daae1637664c11a810230
JorgeBH9 <jorgebh9@outlook.com>
2018-09-18 12:25:25 -0500

    New changes in connection file

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,17 @@ var mysql = require("mysql");
 
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWS_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         host: "localhost",
-//         port: 3306,
-//         user: "root",
-//         password: "root",
-//         database: "burger_db"
-//     });
-// };
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
+};
 
 // Make connection.
 connection.connect(function (err) {
