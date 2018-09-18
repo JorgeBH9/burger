50c91ad67ce9fd6c6fb0039066c08c7a2e7a511f
JorgeBH9 <jorgebh9@outlook.com>
2018-09-18 12:22:44 -0500

    Added the var connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWS_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "root",
-        database: "burger_db"
-    });
-};
+var connection;
+
+// if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWS_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: "root",
+//         password: "root",
+//         database: "burger_db"
+//     });
+// };
 
 // Make connection.
 connection.connect(function (err) {
